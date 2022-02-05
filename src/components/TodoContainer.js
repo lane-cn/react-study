93c002252b45bb293d920c4baa3f9dd6ce256b1e
lane <llu@interprosoft.com>
2022-02-05 21:41:09 +0800

    create components files, demonstrate parent container component

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+class TodoContainer extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello React</h1>
+                <p>I am in React component</p>
+            </div>
+        )
+    }
+}
+export default TodoContainer
