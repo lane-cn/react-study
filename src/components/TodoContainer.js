7164fae31f741efacaa6d26d584ec0e2c7900c05
lane <llu@interprosoft.com>
2022-02-06 17:25:54 +0800

    complete list and item class, use state & props to display data on screen

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,10 +1,29 @@
 import React from "react";
+import TodoList from "./TodoList";
 class TodoContainer extends React.Component {
+    state = {
+        todos: [
+            {
+                id: 1,
+                title: "Setup development environment",
+                completed: true
+            },
+            {
+                id: 2,
+                title: "Develop website and add content",
+                completed: false
+            },
+            {
+                id: 3,
+                title: "Deploy a live site",
+                completed: false
+            }
+        ]
+    };
     render() {
         return (
             <div>
-                <h1>Hello React</h1>
-                <p>I am in React component</p>
+                <TodoList todos={this.state.todos}></TodoList>
             </div>
         )
     }
