345a1805f67b3682bfbdbba5617b0e3bc6a7e03c
lane <llu@interprosoft.com>
2022-02-06 23:58:26 +0800

    style: add styles on screen

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,14 +1,25 @@
 import React from "react";
+import styles from "./TodoItem.module.css";
 class TodoItem extends React.Component {
+    
     render() {
+        const completedStyle = {
+            fontStyle: "italic",
+            color: "#595959",
+            opacity: 0.4,
+            textDecoration: "line-through",
+        }
         return (
-            <li>
+            <li className={styles.item}>
                 <input 
                     type="checkbox" 
                     checked={this.props.todo.completed} 
                     onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+                    className={styles.checkbox}
                 />
-                {this.props.todo.title}
+                <span style={this.props.todo.completed ? completedStyle : null}>
+                    {this.props.todo.title}
+                </span>
                 <button onClick={() => this.props.handleDeleteProps(this.props.todo.id)}>
                     Delete
                 </button>
