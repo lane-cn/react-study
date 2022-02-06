5e51ad70a376300808bda5481fbe6dffebb63d6f
lane <llu@interprosoft.com>
2022-02-06 21:15:24 +0800

    feat: complete update & delete functions

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -21,11 +21,43 @@ class TodoContainer extends React.Component {
             }
         ]
     };
+    handleChange = (id) => {
+        console.log('handleChange: ' + id);
+        this.setState(preState => {
+            return {
+                todos: preState.todos.map(todo => {
+                    if (todo.id === id) {
+                        return {
+                            ...todo,
+                            completed: !todo.completed,
+                        }
+                    }
+                    return todo;
+                })
+            }
+        });
+    };
+    handleDelete = (id) => {
+        console.log('handleDelete: ' + id);
+        this.setState(preState => {
+            return {
+                todos: [
+                    ...this.state.todos.filter(todo => {
+                        return todo.id !== id;
+                    })
+                ]
+            }
+        });
+    };
     render() {
         return (
             <div>
                 <Header></Header>
-                <TodoList todos={this.state.todos}></TodoList>
+                <TodoList 
+                    todos={this.state.todos} 
+                    handleChangeProps={this.handleChange} 
+                    handleDeleteProps={this.handleDelete}
+                />
             </div>
         )
     }
